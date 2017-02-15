1e2d6d3936662acdcac089a7bb7ffe3487d12b34
Victoria <wikishovv@gmail.com>
2017-02-15 09:50:24 +0200

    Channel factory tests added

--- BotAssistant/Core/Channel.cs
+++ BotAssistant/Core/Channel.cs
@@ -28,6 +28,18 @@ namespace BotAssistant.Core
             //for each channel return his own inheritor of channel class
             switch (ChannelId)
             {
+                case "skype":
+                    return new ChannelSkype(connector);
+                case "email":
+                    return new ChannelEmail(connector);
+                case "groupme":
+                    return new ChannelGroupMe(connector);
+                case "kik":
+                    return new ChannelKik(connector);
+                case "slack":
+                    return new ChannelSlack(connector);
+                case "twilio":
+                    return new ChannelTwilio(connector);
                 case "telegram":
                     return new ChannelTelegram(connector);
                 default:
@@ -64,6 +76,116 @@ namespace BotAssistant.Core
             return await connector.Conversations.SendToConversationAsync((Activity)message);
         }
     }
+
+    public class ChannelTwilio : Channel
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="connector">connector client</param>
+        public ChannelTwilio(ConnectorClient connector) : base(connector) { }
+        /// <summary>
+        /// create a new conversation and send a new message
+        /// </summary>
+        /// <param name="User">user account</param>
+        /// <param name="Bot">bot account</param>
+        /// <param name="Text">message text</param>
+        /// <returns>response</returns>
+        public override Task<ResourceResponse> sendNewMessageAcync(ChannelAccount User, ChannelAccount Bot, string Text)
+        {
+            return base.sendNewMessageAcync(User, Bot, Text);
+        }
+    }
+
+    public class ChannelSlack : Channel
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="connector">connector client</param>
+        public ChannelSlack(ConnectorClient connector) : base(connector) { }
+        /// <summary>
+        /// create a new conversation and send a new message
+        /// </summary>
+        /// <param name="User">user account</param>
+        /// <param name="Bot">bot account</param>
+        /// <param name="Text">message text</param>
+        /// <returns>response</returns>
+        public override Task<ResourceResponse> sendNewMessageAcync(ChannelAccount User, ChannelAccount Bot, string Text)
+        {
+            return base.sendNewMessageAcync(User, Bot, Text);
+        }
+    }
+
+    public class ChannelGroupMe : Channel
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="connector">connector client</param>
+        public ChannelGroupMe(ConnectorClient connector) : base(connector) { }
+        /// <summary>
+        /// create a new conversation and send a new message
+        /// </summary>
+        /// <param name="User">user account</param>
+        /// <param name="Bot">bot account</param>
+        /// <param name="Text">message text</param>
+        /// <returns>response</returns>
+        public override Task<ResourceResponse> sendNewMessageAcync(ChannelAccount User, ChannelAccount Bot, string Text)
+        {
+            return base.sendNewMessageAcync(User, Bot, Text);
+        }
+    }
+
+    public class ChannelKik : Channel
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="connector">connector client</param>
+        public ChannelKik(ConnectorClient connector) : base(connector) { }
+        /// <summary>
+        /// create a new conversation and send a new message
+        /// </summary>
+        /// <param name="User">user account</param>
+        /// <param name="Bot">bot account</param>
+        /// <param name="Text">message text</param>
+        /// <returns>response</returns>
+        public override Task<ResourceResponse> sendNewMessageAcync(ChannelAccount User, ChannelAccount Bot, string Text)
+        {
+            return base.sendNewMessageAcync(User, Bot, Text);
+        }
+    }
+
+    public class ChannelEmail : Channel
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="connector">connector client</param>
+        public ChannelEmail(ConnectorClient connector) : base(connector) { }
+    }
+
+    public class ChannelSkype : Channel
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="connector">connector client</param>
+        public ChannelSkype(ConnectorClient connector) : base(connector) { }
+        /// <summary>
+        /// create a new conversation and send a new message
+        /// </summary>
+        /// <param name="User">user account</param>
+        /// <param name="Bot">bot account</param>
+        /// <param name="Text">message text</param>
+        /// <returns>response</returns>
+        public override Task<ResourceResponse> sendNewMessageAcync(ChannelAccount User, ChannelAccount Bot, string Text)
+        {
+            return base.sendNewMessageAcync(User, Bot, Text);
+        }
+    }
+
     /// <summary>
     /// Channel for Telegram
     /// </summary>
---
+++ BotAssistantTest/ChannelTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BotAssistant.Core;
+using Microsoft.Bot.Connector;
+
+namespace BotAssistantTest
+{
+    [TestClass]
+    public class ChannelTests
+    {
+        [TestMethod]
+        public void fabricMethod_ReceiveTelegramChannelId_returnChannelTelegram()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "telegram";
+            // act
+            Channel ch = Channel.fabricMethod(connector,channelId);
+            // assert
+            Assert.IsInstanceOfType(ch,typeof(ChannelTelegram));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveOtherChannelId_returnChannel()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "zxb";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(Channel));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveSkypeChannelId_returnChannelSkype()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "skype";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(ChannelSkype));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveEmailChannelId_returnChannelEmail()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "email";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(ChannelEmail));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveGroupMeChannelId_returnChannelGroupMe()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "groupme";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(ChannelGroupMe));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveKikChannelId_returnChannelKik()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "kik";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(ChannelKik));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveOtherSlack_returnChannelSlack()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "slack";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(ChannelSlack));
+        }
+        [TestMethod]
+        public void fabricMethod_ReceiveTwilioChannelId_returnChannelTwilio()
+        {
+            // arrange
+            ConnectorClient connector = new ConnectorClient(new Uri("http://google.com"));
+            string channelId = "twilio";
+            // act
+            Channel ch = Channel.fabricMethod(connector, channelId);
+            // assert
+            Assert.IsInstanceOfType(ch, typeof(ChannelTwilio));
+        }
+    }
+}
